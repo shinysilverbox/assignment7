8837f929ba9e60fbb3ef6c6c12f86542420f863c
Hobo Noir <hobonetics@gmail.com>
2017-07-12 13:53:54 -0400

    First commit: displayed welcome message.

---
+++ models.py
@@ -0,0 +1,100 @@
+# models.py
+# YOUR NAME(S) AND NETID(S) HERE
+# DATE COMPLETED HERE
+"""Models module for Breakout
+
+This module contains the model classes for the Breakout game. That is anything that you
+interact with on the screen is model: the paddle, the ball, and any of the bricks.
+
+Technically, just because something is a model does not mean there has to be a special 
+class for it.  Unless you need something special, both paddle and individual bricks could
+just be instances of GRectangle.  However, we do need something special: collision 
+detection.  That is why we have custom classes.
+
+You are free to add new models to this module.  You may wish to do this when you add
+new features to your game.  If you are unsure about whether to make a new class or 
+not, please ask on Piazza."""
+import random # To randomly generate the ball velocity
+from constants import *
+from game2d import *
+
+
+# PRIMARY RULE: Models are not allowed to access anything except the module constants.py.
+# If you need extra information from Play, then it should be a parameter in your method, 
+# and Play should pass it as a argument when it calls the method.
+
+
+class Paddle(GRectangle):
+    """An instance is the game paddle.
+    
+    This class contains a method to detect collision with the ball, as well as move it
+    left and right.  You may wish to add more features to this class.
+    
+    The attributes of this class are those inherited from GRectangle.
+    
+    LIST MORE ATTRIBUTES (AND THEIR INVARIANTS) HERE IF NECESSARY
+    """
+    # GETTERS AND SETTERS (ONLY ADD IF YOU NEED THEM)
+    
+    # INITIALIZER TO CREATE A NEW PADDLE
+    
+    # METHODS TO MOVE THE PADDLE AND CHECK FOR COLLISIONS
+    
+    # ADD MORE METHODS (PROPERLY SPECIFIED) AS NECESSARY
+
+
+class Brick(GRectangle):
+    """An instance is the game paddle.
+    
+    This class contains a method to detect collision with the ball.  You may wish to 
+    add more features to this class.
+    
+    The attributes of this class are those inherited from GRectangle.
+    
+    LIST MORE ATTRIBUTES (AND THEIR INVARIANTS) HERE IF NECESSARY
+    """
+    
+    # GETTERS AND SETTERS (ONLY ADD IF YOU NEED THEM)
+    
+    # INITIALIZER TO CREATE A BRICK
+    
+    # METHOD TO CHECK FOR COLLISION
+    
+    # ADD MORE METHODS (PROPERLY SPECIFIED) AS NECESSARY
+
+
+class Ball(GEllipse):
+    """Instance is a game ball.
+    
+    We extend GEllipse because a ball must have additional attributes for velocity.
+    This class adds this attributes and manages them.
+    
+    INSTANCE ATTRIBUTES:
+        _vx [int or float]: Velocity in x direction 
+        _vy [int or float]: Velocity in y direction 
+    
+    The class Play will need to look at these attributes, so you will need
+    getters for them.  However, it is possible to write this assignment with no
+    setters for the velocities.
+    
+    How? The only time the ball can change velocities is if it hits an obstacle
+    (paddle or brick) or if it hits a wall.  Why not just write methods for these
+    instead of using setters?  This cuts down on the amount of code in Gameplay.
+    
+    NOTE: The ball does not have to be a GEllipse. It could be an instance
+    of GImage (why?). This change is allowed, but you must modify the class
+    header up above.
+    
+    LIST MORE ATTRIBUTES (AND THEIR INVARIANTS) HERE IF NECESSARY
+    """
+    
+    # GETTERS AND SETTERS (ONLY ADD IF YOU NEED THEM)
+    
+    # INITIALIZER TO SET RANDOM VELOCITY
+    
+    # METHODS TO MOVE AND/OR BOUNCE THE BALL
+    
+    # ADD MORE METHODS (PROPERLY SPECIFIED) AS NECESSARY
+
+
+# IF YOU NEED ADDITIONAL MODEL CLASSES, THEY GO HERE
---
+++ play.py
@@ -0,0 +1,65 @@
+# play.py
+# YOUR NAME(S) AND NETID(S) HERE
+# DATE COMPLETED HERE
+"""Subcontroller module for Breakout
+
+This module contains the subcontroller to manage a single game in the Breakout App. 
+Instances of Play represent a single game.  If you want to restart a new game, you are 
+expected to make a new instance of Play.
+
+The subcontroller Play manages the paddle, ball, and bricks.  These are model objects.  
+Their classes are defined in models.py.
+
+Most of your work on this assignment will be in either this module or models.py.
+Whether a helper method belongs in this module or models.py is often a complicated
+issue.  If you do not know, ask on Piazza and we will answer."""
+from constants import *
+from game2d import *
+from models import *
+
+
+# PRIMARY RULE: Play can only access attributes in models.py via getters/setters
+# Play is NOT allowed to access anything in breakout.py (Subcontrollers are not
+# permitted to access anything in their parent. To see why, take CS 3152)
+
+class Play(object):
+    """An instance controls a single game of breakout.
+    
+    This subcontroller has a reference to the ball, paddle, and bricks. It animates the 
+    ball, removing any bricks as necessary.  When the game is won, it stops animating.  
+    You should create a NEW instance of Play (in Breakout) if you want to make a new game.
+    
+    If you want to pause the game, tell this controller to draw, but do not update.  See 
+    subcontrollers.py from Lecture 25 for an example.
+    
+    INSTANCE ATTRIBUTES:
+        _paddle [Paddle]: the paddle to play with 
+        _bricks [list of Brick]: the list of bricks still remaining 
+        _ball   [Ball, or None if waiting for a serve]:  the ball to animate
+        _tries  [int >= 0]: the number of tries left 
+    
+    As you can see, all of these attributes are hidden.  You may find that you want to
+    access an attribute in class Breakout. It is okay if you do, but you MAY NOT ACCESS 
+    THE ATTRIBUTES DIRECTLY. You must use a getter and/or setter for any attribute that 
+    you need to access in Breakout.  Only add the getters and setters that you need for 
+    Breakout.
+    
+    You may change any of the attributes above as you see fit. For example, you may want
+    to add new objects on the screen (e.g power-ups).  If you make changes, please list
+    the changes with the invariants.
+                  
+    LIST MORE ATTRIBUTES (AND THEIR INVARIANTS) HERE IF NECESSARY
+    """
+    
+    
+    # GETTERS AND SETTERS (ONLY ADD IF YOU NEED THEM)
+    
+    # INITIALIZER (standard form) TO CREATE PADDLES AND BRICKS
+    
+    # UPDATE METHODS TO MOVE PADDLE, SERVE AND MOVE THE BALL
+    
+    # DRAW METHOD TO DRAW THE PADDLES, BALL, AND BRICKS
+    
+    # HELPER METHODS FOR PHYSICS AND COLLISION DETECTION
+    
+    # ADD ANY ADDITIONAL METHODS (FULLY SPECIFIED) HERE
